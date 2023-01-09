fcc814cf76e14e5415750fdb5d59fd3132680ba3
AnimeHacker <jhankandry1899@gmail.com>
2023-01-09 13:47:06 -0500

    Inicio del proyecto

---
+++ database/migrations/2023_01_09_183104_create_cars_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('cars', function (Blueprint $table) {
+            $table->id();
+            //RELACION
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users');
+
+            $table->string('title')->unique();
+            $table->string('description');
+            $table->double('price');
+            $table->string('status');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('cars');
+    }
+};
